3bab7c97bc7857976897514dafe85bce22552619
amandwivedi0 <anshuman.p01999@gmail.com>
2020-07-18 04:52:29 +0000

    add 720x1280 bootanimation

--- bootanimation.mk
+++ bootanimation.mk
@@ -1,24 +1,28 @@
+#
+# Copyright (C) 2018 The Dirty Unicorns Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 # Boot Animation
-
-# Black & White
-TARGET_MISC_BLOCK_OFFSET ?= 0
-
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.misc.block.offset=$(TARGET_MISC_BLOCK_OFFSET)
-
-PRODUCT_PACKAGES += \
-    boot_theme \
-    misc_writer_system
-
-scr_resolution := 1080
+scr_resolution := 720x1280
 du_device := $(patsubst %f,%,$(subst du_,,$(TARGET_PRODUCT)))
 
-ifneq ($(filter crosshatch coral dragon emulator marlin taimen,$(du_device)),)
-scr_resolution := 1440
+ifneq ($(filter santoni,$(du_device)),)
+scr_resolution := 720x1280
 endif
 
 ifneq ($(wildcard vendor/themes/bootanimation/$(scr_resolution).zip),)
 PRODUCT_COPY_FILES += \
-    vendor/themes/bootanimation/$(scr_resolution).zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip \
-    vendor/themes/bootanimation/$(scr_resolution)-dark.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation-dark.zip
+    vendor/themes/bootanimation/$(scr_resolution).zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 endif
